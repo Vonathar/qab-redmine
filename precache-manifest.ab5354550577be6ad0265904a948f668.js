c1ab8a410d6e99883317aa5d720cf95f919e64fd
Gianmarco <gianmarco.caputo@wearetwogether.com>
2020-01-21 17:00:54 +0000

    Updates

---
+++ precache-manifest.ab5354550577be6ad0265904a948f668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d40d5eedd8bc0199f6387e8cdb91d14",
+    "url": "/qab-redmine/index.html"
+  },
+  {
+    "revision": "6cc7afb30bc19317c8ac",
+    "url": "/qab-redmine/static/css/main.f2daf9a9.chunk.css"
+  },
+  {
+    "revision": "31ff2a875ba394fcfb2b",
+    "url": "/qab-redmine/static/js/2.cacb5c1c.chunk.js"
+  },
+  {
+    "revision": "aa4bef56bac67837f3cf7d3b9abea3f8",
+    "url": "/qab-redmine/static/js/2.cacb5c1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6cc7afb30bc19317c8ac",
+    "url": "/qab-redmine/static/js/main.1ff52560.chunk.js"
+  },
+  {
+    "revision": "116147794e0d590808ee",
+    "url": "/qab-redmine/static/js/runtime-main.c73ebeae.js"
+  }
+]);
